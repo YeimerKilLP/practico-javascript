5ff3db1ed9e37e43a06aa3ed2bdcdf7b624f6774
Jimmy Perez <jimmylopez375@gmail.com>
2021-09-25 21:03:55 -0500

    Se estan creando las funciones

--- figuras.js
+++ figuras.js
@@ -1,9 +1,13 @@
 //Codigo del cuadrado
 console.group ('Cuadrado');
-const ladoCuadrado = 5;
-console.log ('Los lados del cuadrado miden: ' +ladoCuadrado + 'cm');
+//const ladoCuadrado = 5;
+//console.log ('Los lados del cuadrado miden: ' +ladoCuadrado + 'cm');
 
-const perimetroCuadrado = ladoCuadrado * 4;
+function  perimetroCuadrado (lado){
+return lado * 4;
+}; 
+perimetroCuadrado( );
+
 console.log ('El perimetro del cuadrado mide: ' +perimetroCuadrado+ 'cm' );
 
 const areaCuadrado = ladoCuadrado * ladoCuadrado;
@@ -32,4 +36,29 @@ console.groupEnd ();
 
 //Circulos 
 console.group('Circulo');
+
+//Radio
+
+const radioCirculo = 4;
+
+console.log ('El radio del circulo es '+ radioCirculo + 'cm');
+
+//Diametro
+
+const diametroCirculo = radioCirculo *2;
+console.log(`El radio del circulo es ${diametroCirculo}cm`);
+
+//PI
+
+const PI = Math.PI;
+console.log (`El PI del circulo es ${PI}cm`);
+//Circunferencia
+
+const perimetroCirculo = diametroCirculo * PI;
+console.log (`El PI del circulo es ${perimetroCirculo}cm`);
+//Area
+
+const areaCirculo = (radioCirculo* radioCirculo) * PI; 
+console.log (`El area del circulo es ${areaCirculo}cm`)
 console.groupEnd();
+
