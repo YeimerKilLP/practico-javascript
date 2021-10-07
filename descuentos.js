06ae0381e450b21b6384c632becaca2ae904829b
Jimmy Perez <jimmylopez375@gmail.com>
2021-10-06 21:18:02 -0500

    6 octubre

--- descuentos.js
+++ descuentos.js
@@ -1,6 +1,7 @@
+
 function calcularPrecioConDescuento (precio, descuento) {
 
-const porcentajeDelPrecioConDescuento = 100 - descuento;
+let porcentajeDelPrecioConDescuento = 100 - descuento;
 
 const precioFinal = (precio * porcentajeDelPrecioConDescuento)/100; 
 
@@ -8,7 +9,7 @@ const precioFinal = (precio * porcentajeDelPrecioConDescuento)/100;
 return precioFinal;
 }
 
-
+/* 
 
 function onclickButtonPriceDiscount(){
     const intputPrice =document.getElementById ("InputPrice");
@@ -23,3 +24,58 @@ function onclickButtonPriceDiscount(){
     resultP.innerText = `El precio con descuento es $: ${precioConDescuento}`
 
 }
+
+
+ */
+
+
+
+//Cupones  
+const cupons  = [ "CHEEMS", "JIMMY" , "HONOR", "FORNAI"];
+/* 
+const inputCupon = document.getElementById ("InputCupon");
+const cuponValue = inputCupon.value; */
+
+function onclickButtonCupon(){
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+
+    const inputCupon = document.getElementById ("InputCupon");
+    const cuponValue = inputCupon.value;
+
+    
+
+    let descuento; 
+
+    console.log(inputPrice,cuponValue);
+
+
+    switch (cuponValue){
+        case cupons [0]:
+            descuento =15; //Cheems
+            break;
+            
+        case cupons [1]:
+            descuento =30; //Jimmy
+            break;
+        
+        case cupons [2]:
+            descuento= 45; //HONOR
+            break;
+
+        case cupons[3]:
+            descuento = 60; //FORNAI
+            break;
+    }
+
+    const precioConDescuento =  calcularPrecioConDescuento (priceValue, cuponValue);
+
+    const resultP = document.getElementById ("ResultP");
+    resultP.innerText = `El precio con descuento es $: ${precioConDescuento}`
+
+}
+
+
+
+
+
