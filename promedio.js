1a5561601812afc331ee4b12fa44c53a31a075a7
Jimmy Perez <jimmylopez375@gmail.com>
2021-10-10 15:05:45 -0500

    El nombre de la funcion estaba incorrecto.

--- promedio.js
+++ promedio.js
@@ -38,15 +38,46 @@ function addLista() {
 si es correcto pulsa el boton de calcular promedio`;
 }
 
+//Eliminar valor
+
+function deletevalor() {
+  const inputNumero = document.getElementById("inputNumero");
+
+  //Se obtiene los datos del inputNumero del HTML
+
+  const numero = Number(inputNumero.value);
+
+  const delnum = listaNum.pop(numero);
+
+  const msjLog = document.getElementById("msjLog1");
+  msjLog.innerText = `${delnum} ha sido elimado de la lista`;
+}
+
+/* 
 function calculaPromedio() {
-  const sumaLista = listaNum.reduce(function (valorAcumulado = 0, nuevoElemento) {
+  const sumaLista = listaNum.reduce(function (
+    valorAcumulado = 0,
+    nuevoElemento
+  ) {
     return valorAcumulado + nuevoElemento;
   });
 
   const promedio = sumaLista / listaNum.length;
 
-  const msjLog1 = document.getElementById("msjLog1");
-  msjLog1.innertext = `El promedio es ${promedio}`; 
+  const resultado = document.getElementById("resultadoFinal");
+  resultado.innertext = `El promedio es ${promedio}`;
+} */
+function calculaPromedio() {
+  const sumalista = listaNum.reduce(function (
+    valorAcumulado = 0,
+    nuevoElemento
+  ) {
+    return valorAcumulado + nuevoElemento;
+  });
+  const promedioLista = sumalista / listaNum.length;
 
+  const resultadoFinal = document.getElementById("resultadoFinal");
+  resultadoFinal.innerText = "El promedio es " + promedioLista;
 }
-console.log (msjLog1.innerText);
+
+console.log(promedioLista);
